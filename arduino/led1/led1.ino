806f65a3b29db8b09b8fdb54b0826c64ecf51cc9
Gustavo <andresbucheli352@gmail.com>
2023-09-08 12:06:34 -0500

    led hello

---
+++ arduino/led1/led1.ino
@@ -0,0 +1,14 @@
+//gustavo rosero
+int led=13;
+void setup() {
+  // put your setup code here, to run once:
+pinMode(led,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(led,HIGH);
+  delay(1000);//1000s
+  digitalWrite(led,LOW);
+  delay(1000);//1000s
+}
