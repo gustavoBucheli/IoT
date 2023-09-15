1e1365ab8853eadc580c27aa05490d04b7e96112
gustavoBucheli <143829497+gustavoBucheli@users.noreply.github.com>
2023-09-15 13:46:39 -0500

    Update reto2.ino

--- arduino/reto2/reto2.ino
+++ arduino/reto2/reto2.ino
@@ -1,26 +1,26 @@
-int led_verd=13;
-int led_amar=11;
-int led_rojo=10;
+int verde=13;
+int naranja=11;
+int rojo=10;
 void setup() {
   // put your setup code here, to run once:
-  pinMode(led_verd,OUTPUT);//No Pin y Mode (OUTPUT/INPUT)
-  pinMode(led_amar,OUTPUT);
-  pinMode(led_rojo,OUTPUT);
+  pinMode(verde,OUTPUT);
+  pinMode(naranja,OUTPUT);
+  pinMode(rojo,OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   
-  digitalWrite(led_rojo,HIGH);
-  delay(500); //1000=1 SEG
-  digitalWrite(led_rojo,LOW);
-  delay(1000); //1000=1 SEG
-  digitalWrite(led_amar,HIGH);
-  delay(500); //1000=1 SEG
-  digitalWrite(led_amar,LOW);
-  delay(1000); //1000=1 SEG
-  digitalWrite(led_verd,HIGH);
-  delay(500); //1000=1 SEG
-  digitalWrite(led_verd,LOW);
-  delay(1000); //1000=1 SEG
+  digitalWrite(rojo,HIGH);
+  delay(500); 
+  digitalWrite(rojo,LOW);
+  delay(1000); 
+  digitalWrite(naranja,HIGH);
+  delay(500); 
+  digitalWrite(naranja,LOW);
+  delay(1000); 
+  digitalWrite(verde,HIGH);
+  delay(500); 
+  digitalWrite(verde,LOW);
+  delay(1000); 
 }
